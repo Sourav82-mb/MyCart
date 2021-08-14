5e8de045ce584a87732837f00d9c7dd0e4cc023b
Sourav82-mb <ssoourav719@gmail.com>
2021-08-14 10:44:29 +0530

    Change for live server

--- MyCart/settings.py
+++ MyCart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&q059#$n@drhh&=ga^80tj(-9v8cu66gb#_2fgg1)jv3f=&$z0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
