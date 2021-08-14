27b3b3bc8f6ba07dd4377556f69f728be6f78d10
Sourav82-mb <ssoourav719@gmail.com>
2021-08-14 10:59:23 +0530

    url

--- MyCart/settings.py
+++ MyCart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&q059#$n@drhh&=ga^80tj(-9v8cu66gb#_2fgg1)jv3f=&$z0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','ccmy.herokuapp.com']
 
 
 # Application definition
